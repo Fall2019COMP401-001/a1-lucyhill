1ee4dec6583e491cbc54707566c230c66fdf71c3
Lucy <lucyadhill@gmail.com>
2019-09-01 15:14:00 -0400

    changed some things

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -64,7 +64,7 @@ public class A1Jedi {
 		for (int i=0; i<customer.items.length; i++) {
 				for (int x=0; x<items.length; x++) {
 					if (customer.items[i].name.contentEquals(items[x].name)) {
-						items[i].custNum++;
+						items[i].cust++;
 					}
 				}
 		}
